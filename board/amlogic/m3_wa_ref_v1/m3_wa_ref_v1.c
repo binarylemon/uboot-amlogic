021c12027b49c631d1ba574ce2c22ac0d2edf281
jiamin.miao <jiamin.miao@amlogic.com>
2012-03-15 14:04:16 +0800

    update nand driver

--- board/amlogic/m3_wa_ref_v1/m3_wa_ref_v1.c
+++ board/amlogic/m3_wa_ref_v1/m3_wa_ref_v1.c
@@ -246,7 +246,7 @@ static struct aml_nand_platform aml_nand_mid_platform[] = {
         .platform_nand_data = {
             .chip =  {
                 .nr_chips = 1,
-                .options = (NAND_TIMING_MODE5| NAND_ECC_BCH30_MODE),
+                .options = (NAND_TIMING_MODE5| NAND_ECC_BCH30_1K_MODE),
             },
         },
         .rbpin_mode = 1,
--- board/amlogic/m6_ref_v1/m6_ref_v1.c
+++ board/amlogic/m6_ref_v1/m6_ref_v1.c
@@ -340,7 +340,7 @@ static struct aml_nand_platform aml_nand_mid_platform[] = {
         .platform_nand_data = {
             .chip =  {
                 .nr_chips = 1,
-                .options = (NAND_TIMING_MODE5 | NAND_ECC_SHORT_MODE),
+                .options = (NAND_TIMING_MODE5 | NAND_ECC_BCH30_1K_MODE),
             },
         },
         .rbpin_mode=1,
@@ -356,7 +356,7 @@ static struct aml_nand_platform aml_nand_mid_platform[] = {
         .platform_nand_data = {
             .chip =  {
                 .nr_chips = 1,
-                .options = (NAND_TIMING_MODE5 | NAND_ECC_BCH30_MODE | NAND_TWO_PLANE_MODE),
+                .options = (NAND_TIMING_MODE5 | NAND_ECC_BCH30_1K_MODE | NAND_TWO_PLANE_MODE),
             },
         },
         .rbpin_mode = 1,
