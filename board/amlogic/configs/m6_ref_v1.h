6c3b7c2e7aec69078eaa971d4569d5da70be987f
elvis.yu <elvis.yu@amlogic.com>
2012-04-01 15:39:39 +0800

    define CONFIG_CMD_AUTOSCRIPT for M6 ref & ramos

--- board/amlogic/configs/m6_ref_v1.h
+++ board/amlogic/configs/m6_ref_v1.h
@@ -78,6 +78,7 @@
 #define CONFIG_UCL 1
 #define CONFIG_SELF_COMPRESS
 
+#define CONFIG_CMD_AUTOSCRIPT
 #define CONFIG_CMD_AML 1
 #define CONFIG_CMD_IMGPACK 1
 #define CONFIG_CMD_REBOOT 1
@@ -115,7 +116,7 @@
 	"upgrade_check=if itest ${upgrade_step} == 0; then defenv; save; run update; else if itest ${upgrade_step} == 1; then defenv; set upgrade_step 2; save; fi; fi\0" \
 	"switch_bootmode=get_rebootmode; clear_rebootmode; echo reboot_mode=${reboot_mode}; if test ${reboot_mode} = normal; then run prepare; else if test ${reboot_mode} = factory_reset; then run recovery; else if test ${reboot_mode} = update; then run update; else run charging_or_not; fi; fi; fi\0" \
 	"prepare=nand read logo ${loadaddr_misc} 0 40000; unpackimg ${loadaddr_misc}; video open; bmp display ${poweron_offset}\0" \
-	"update=bmp display ${bootup_offset}; if mmcinfo; then if fatload mmc 0 ${loadaddr} uImage_recovery; then bootm; if nand read recovery ${loadaddr} 0 400000; then bootm; fi; fi; fi\0" \
+	"update=bmp display ${bootup_offset}; if mmcinfo; then if fatload mmc 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi; if fatload mmc 0 ${loadaddr} uImage_recovery; then bootm; fi; fi; nand read recovery ${loadaddr} 0 400000; bootm\0" \
 	"recovery=bmp display ${bootup_offset}; if nand read recovery ${loadaddr} 0 400000; then bootm; else echo no uImage_recovery in NAND; fi\0" \
 	"charging_or_not=if ac_online; then run prepare; run charing; else if getkey; then run prepare; run bootcmd; else poweroff; fi; fi\0" \
 	"charing=video clear; run display_loop\0" \
