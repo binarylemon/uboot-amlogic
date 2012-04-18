9c27113d075d23a9b9738d3d7fd9e21472e7d666
elvis.yu <elvis.yu@amlogic.com>
2012-04-18 20:10:05 +0800

    For M6 Ramos Board, speed up Power key hold, so fast for boot up.

--- customer/board/m6_ramos_v1/m6_ramos_v1.c
+++ customer/board/m6_ramos_v1/m6_ramos_v1.c
@@ -433,6 +433,7 @@ int board_init(void)
 #ifdef	BOARD_LATE_INIT
 int board_late_init(void)
 {
+	uint8_t reg;
 #ifdef CONFIG_AML_I2C  
 	board_i2c_init();
 #endif /*CONFIG_AML_I2C*/
@@ -441,6 +442,15 @@ int board_late_init(void)
 	board_usb_init(&g_usb_config_m6_skt);
 #endif /*CONFIG_USB_DWC_OTG_HCD*/
 
+#ifdef CONFIG_AW_AXP20  
+#define POWER20_PEK_SET             (0x36)
+
+	axp_read(POWER20_PEK_SET, &reg);
+	printf("0: reg=0x%x\n", reg);
+	reg &= ~(3<<6);
+	axp_write(POWER20_PEK_SET, reg);
+	printf("1: reg=0x%x\n", reg);
+#endif /*CONFIG_AW_AXP20*/
 	return 0;
 }
 #endif
