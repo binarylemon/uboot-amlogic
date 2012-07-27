70dad7a5c8d303c2578146a21f3b6b0411a0774f
elvis.yu <elvis.yu@amlogic.com>
2012-07-27 11:29:58 +0800

    optimze USB burning process

--- arch/arm/cpu/aml_meson/m6/firmware/usb_boot/usb_boot.c
+++ arch/arm/cpu/aml_meson/m6/firmware/usb_boot/usb_boot.c
@@ -1,3 +1,5 @@
+#include <asm/arch/reboot.h>
+
 #include "platform.h"
 #include "usb_pcd.h"
 
@@ -30,6 +32,7 @@ int usb_boot(int clk_cfg)
 void relocate_init(unsigned __TEXT_BASE,unsigned __TEXT_SIZE)
 {
 	writel(0,P_WATCHDOG_TC);//disable Watchdog
+	reboot_mode = MESON_USB_BURNER_REBOOT;
 	while(1)
 	{
 		//usb_boot(0);	//Elvis
