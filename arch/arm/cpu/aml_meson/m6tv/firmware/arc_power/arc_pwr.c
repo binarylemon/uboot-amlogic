3457b58ac7ee0a1f27ab88dccaa80f76ed7170b5
jacky <jionggan.xie@amlogic.com>
2013-02-15 11:41:44 +0800

    chang remoter flag REG1 to REG2 for match kernel

--- arch/arm/cpu/aml_meson/m6tv/firmware/arc_power/arc_pwr.c
+++ arch/arm/cpu/aml_meson/m6tv/firmware/arc_power/arc_pwr.c
@@ -325,7 +325,7 @@ void enter_power_down()
 		  power_key = (power_key>>16)&0xff;
 
 		  if(power_key==0x10 || power_key==0x0c){
-				  writel(0x1234abcd,P_AO_RTI_STATUS_REG1);
+				  writel(0x1234abcd,P_AO_RTI_STATUS_REG2);
 				  f_serial_puts("remote power key\n");
 				  //the reference remote power key code
 	        	  break;
