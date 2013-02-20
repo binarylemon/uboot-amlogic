8053e610b597951af8ef5a707e1c2a3644ca1cbe
Geng Li <geng.li@amlogic.com>
2013-02-20 13:50:49 +0800

    [G19]Modify nand struct

--- customer/board/m6_duokan_mbx_g19ref/m6_duokan_mbx_g19ref.c
+++ customer/board/m6_duokan_mbx_g19ref/m6_duokan_mbx_g19ref.c
@@ -341,7 +341,7 @@ void    board_nand_init(void)
 
 
 static struct aml_nand_platform aml_nand_mid_platform[] = {
- /* 
+#ifdef CONFIG_NAND_BOOT
     {
         .name = NAND_BOOT_NAME,
         .chip_enable_pad = AML_NAND_CE0,
@@ -352,26 +352,23 @@ static struct aml_nand_platform aml_nand_mid_platform[] = {
                 .options = (NAND_TIMING_MODE5 | NAND_ECC_BCH30_1K_MODE),
             },
         },
-        .rbpin_mode=1,
-        .short_pgsz=384,
-        .ran_mode=0,
+        .rbpin_detect = 1,
         .T_REA = 20,
         .T_RHOH = 15,
     },
-*/    
+#endif 
     {
         .name = NAND_NORMAL_NAME,
-        .chip_enable_pad = (AML_NAND_CE0) | (AML_NAND_CE1 << 4),// | (AML_NAND_CE2 << 8) | (AML_NAND_CE3 << 12)),
-        .ready_busy_pad = (AML_NAND_CE0) | (AML_NAND_CE1 << 4),// | (AML_NAND_CE1 << 8) | (AML_NAND_CE1 << 12)),
+        .chip_enable_pad = (AML_NAND_CE0 /* | (AML_NAND_CE1 << 4) | (AML_NAND_CE2 << 8) | (AML_NAND_CE3 << 12)*/),
+        .ready_busy_pad = (AML_NAND_CE0 /* | (AML_NAND_CE1 << 4) | (AML_NAND_CE1 << 8) | (AML_NAND_CE1 << 12)*/),
         .platform_nand_data = {
             .chip =  {
-                .nr_chips = 2,
+                .nr_chips = 1,
                 .options = (NAND_TIMING_MODE5 | NAND_ECC_BCH60_1K_MODE | NAND_TWO_PLANE_MODE),
             },
         },
-        .rbpin_mode = 1,
-        .short_pgsz = 0,
-        .ran_mode = 0,
+        .rbpin_detect = 1,
+        .ran_mode = 1,
         .T_REA = 20,
         .T_RHOH = 15,
     }
