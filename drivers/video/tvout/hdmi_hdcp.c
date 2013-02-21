76482878fe164f0c2fd889d0e9754d58bbc76b7e
Zongdong Jiao <zongdong.jiao@amlogic.com>
2013-02-21 16:32:12 +0800

    hdmitx: hdcp: pre-clear ksv ram to zeroes

--- drivers/video/tvout/hdmi_hdcp.c
+++ drivers/video/tvout/hdmi_hdcp.c
@@ -36,7 +36,7 @@ static void check_cts_hdmi_sys_clk_status(void)
 
     for(i = 0; i < GATE_NUM; i++){
         if(!(READ_CBUS_REG(hdmi_gate[i].cbus_addr) & (1<<hdmi_gate[i].gate_bit))){
-            printf("HDMI Gate Clock is off, turn on now\n");
+//            printf("HDMI Gate Clock is off, turn on now\n");
             WRITE_CBUS_REG_BITS(hdmi_gate[i].cbus_addr, 1, hdmi_gate[i].gate_bit, 1);
         }
     }
@@ -61,5 +61,21 @@ void hdmi_hdcp_wr_reg(unsigned long addr, unsigned long data)
     WRITE_APB_REG(HDMI_ADDR_PORT, addr);
 
     WRITE_APB_REG(HDMI_DATA_PORT, data);
+}
+
+#define TX_HDCP_KSV_OFFSET          0x540
+#define TX_HDCP_KSV_SIZE            5
+// Must be done by system init
+// In kenrel hdmi driver, it will get AKSV value
+// If equals to 0, then kernel won't enable HDCP
+extern int hdmi_hdcp_clear_ksv_ram(void);
+int hdmi_hdcp_clear_ksv_ram(void)
+{
+    int i;
+    for(i = 0; i < TX_HDCP_KSV_SIZE; i++) {
+        hdmi_hdcp_wr_reg(TX_HDCP_KSV_OFFSET + i, 0x00);
+    }
+    printf("Clear HDMI KSV RAM\n");
+    return 0;
 }
 
