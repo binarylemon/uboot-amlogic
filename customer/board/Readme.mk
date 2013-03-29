04e046cae636ca0fd1f1c53093a33b4b49b906e3
wenbiao.zhang <wenbiao.zhang@amlogic.com>
2013-03-29 16:16:35 +0800

    PD #71598: add config for dongle board with wifi 212

--- customer/board/Readme.mk
+++ customer/board/Readme.mk
@@ -53,6 +53,10 @@ help_customer:
 	@echo ----------------------------------------------------------------------
 	@echo The mark in board is "MBX_AML8726_MX_REF_A"
 	@echo config command: \"make m6_mbx_v1_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "DONGLE_AML8726_MX_REF_A"
+	@echo config command: \"make m6_dongle_config\"
 	
 	@echo ----------------------------------------------------------------------
 	@echo The mark in board is "MBX_AML8726_MX_REF_A"
