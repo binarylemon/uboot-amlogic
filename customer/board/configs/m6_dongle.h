f47ca219b0cab5f2375e4072e113400ed6d7657c
wenbiao.zhang <wenbiao.zhang@amlogic.com>
2013-04-28 11:10:15 +0800

    PD #71991: dongle mistake fi in ubootenv update

--- customer/board/configs/m6_dongle.h
+++ customer/board/configs/m6_dongle.h
@@ -173,7 +173,7 @@
 	"cvbsenable=false\0" \
 	"preboot=get_rebootmode; clear_rebootmode; echo reboot_mode=${reboot_mode}; if test ${reboot_mode} = usb_burning; then tiny_usbtool 20000; fi; run upgrade_check; run check_boot_mode; run switch_bootmode\0" \
 	"upgrade_check=if itest ${upgrade_step} == 1; then defenv_without reboot_mode;setenv upgrade_step 2; save; fi\0" \
-	"update=if mmcinfo; then if fatload mmc 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; if usb start; then if fatload usb 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi;fi;run recovery\0" \
+	"update=if mmcinfo; then if fatload mmc 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr};fi;fi; if usb start; then if fatload usb 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi;fi;run recovery\0" \
 	"updatekey_or_not=saradc open 4;if saradc get_in_range 0x0 0x50 ;then msleep 500;if saradc get_in_range 0x0 0x50; then run update; fi; fi\0" \
 	"check_boot_mode=saradc open 4;if saradc get_in_range 0x0 0x50 ;then msleep 200;if saradc get_in_range 0x0 0x50; then msleep 200; if saradc get_in_range 0x0 0x50; then msleep 3000; if saradc get_in_range 0x0 0x50; then setenv reboot_mode update; fi; fi; if saradc get_in_range 0x3af 0x3ff; then tiny_usbtool 20000; fi; fi; fi\0" \
 	"cvbscheck=setenv outputtemp ${outputmode};if test ${outputmode} = 480i; then if test ${cvbsenable} = true; then setenv outputtemp 480cvbs;fi;fi; if test ${outputmode} = 576i; then if test ${cvbsenable} = true; then setenv outputtemp 576cvbs;fi;fi\0" \
