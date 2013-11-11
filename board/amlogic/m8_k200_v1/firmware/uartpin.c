2c1f0a82b86ce9b182edbbd69f70f719b01469ee
Larson Jiang <larson.jiang@amlogic.com>
2013-11-11 12:49:10 +0800

    fix sdio debug board can not input in uboot

--- board/amlogic/m8_k200_v1/firmware/uartpin.c
+++ board/amlogic/m8_k200_v1/firmware/uartpin.c
@@ -7,6 +7,6 @@ SPL_STATIC_FUNC int serial_set_pin_port(unsigned port_base)
 {
     //UART in "Always On Module"
     //GPIOAO_0==tx,GPIOAO_1==rx
-    setbits_le32(P_AO_RTI_PIN_MUX_REG,3<<11);
+    //setbits_le32(P_AO_RTI_PIN_MUX_REG,3<<11);
     return 0;
 }
