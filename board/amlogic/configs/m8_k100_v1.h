911475cbf80caaa6b3293aafe188bc4a1679d089
Sandy lUo <sandy.luo@amlogic.com>
2013-12-23 09:54:04 +0800

    close 3 macro

--- board/amlogic/configs/m8_k100_v1.h
+++ board/amlogic/configs/m8_k100_v1.h
@@ -118,7 +118,7 @@
  */
 #define CONFIG_POWER_SPL                            // init power for all domians, must have
 #define CONFIG_VCCK_VOLTAGE             1050        // CPU core voltage when boot, must have
-#define CONFIG_VDDAO_VOLTAGE            1150        // VDDAO voltage when boot, must have
+#define CONFIG_VDDAO_VOLTAGE            1100        // VDDAO voltage when boot, must have
 #define CONFIG_DDR_VOLTAGE              1500        // DDR voltage when boot, must have
 
 #define CONFIG_VDDIO_AO28               2900        // VDDIO_AO28 voltage when boot, option
@@ -451,17 +451,17 @@
 #define CONFIG_M8_NO_DDR_PUB_VT_CHECK 1
 
 //For M8 DDR clock gating disable
-#define CONFIG_M8_GATEACDDRCLK_DISABLE 1
+//#define CONFIG_M8_GATEACDDRCLK_DISABLE 1
 
 //For M8 DDR low power feature disable
-#define CONFIG_M8_DDR_LOW_POWER_DISABLE 1
+//#define CONFIG_M8_DDR_LOW_POWER_DISABLE 1
 
 //For M8 DDR PUB WL/WD/RD/RG-LVT, WD/RD-BVT disable
-#define CONFIG_M8_PUB_WLWDRDRGLVTWDRDBVT_DISABLE 1
+//#define CONFIG_M8_PUB_WLWDRDRGLVTWDRDBVT_DISABLE 1
 
 //Please just define m8 DDR clock here only
 //current DDR clock range (408~804)MHz with fixed step 12MHz
-#define CFG_M8_DDR_CLK    696 //768  //792// (636)
+#define CFG_M8_DDR_CLK    792//696 //768  //792// (636)
 
 //On board DDR capactiy
 //#define CFG_M8_DDR3_1GB
