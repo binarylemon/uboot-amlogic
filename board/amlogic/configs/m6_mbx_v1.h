0fdcc7feb5b0262022722b7d55768b248aebec4f
xu xing <xing.xu@amlogic.com>
2014-02-08 17:35:42 +0800

    add dts support for g18

--- board/amlogic/configs/m6_mbx_v1.h
+++ board/amlogic/configs/m6_mbx_v1.h
@@ -24,6 +24,9 @@
 #define CONFIG_AML_NAND_KEY
 #define CONFIG_AML_EMMC_KEY	1
 #endif
+/* Pass open firmware flat tree */
+#define CONFIG_OF_LIBFDT    1
+#define CONFIG_SYS_BOOTMAPSZ   PHYS_MEMORY_SIZE       /* Initial Memory map for Linux */
 
 //#define CONFIG_SWITCH_BOOT_MODE 1
 #define CONFIG_HDCP_PREFETCH 1
