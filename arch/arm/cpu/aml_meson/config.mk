99610ba755c719d21c660f03bcf30e057dcd763d
xiaobo.gu <xiaobo.gu@amlogic.com>
2014-07-17 11:42:15 +0800

    M6: Update UCL_TEXT_BASE setting

--- arch/arm/cpu/aml_meson/config.mk
+++ arch/arm/cpu/aml_meson/config.mk
@@ -3,5 +3,5 @@ ARM_CPU=cortex-a9
 PLATFORM_CPPFLAGS += $(call cc-option,-mcpu=cortex-a9  -ffixed-r8 -mno-long-calls  -Wall -fPIC )
 #USE_PRIVATE_LIBGCC=yes
 TEXT_BASE=$(CONFIG_SYS_TEXT_BASE)
-UCL_TEXT_BASE=$(CONFIG_SYS_TEXT_BASE)+0x800000
+UCL_TEXT_BASE=$(CONFIG_SYS_UCL_TEXT_BASE)
 #$(warning $(PLATFORM_CPPFLAGS))
