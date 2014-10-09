6140cb12e171c24564b160aff8187469103e4317
Larson Jiang <larson.jiang@amlogic.com>
2014-10-09 10:57:13 +0800

    fix poc error bug

--- include/partition_table.h
+++ include/partition_table.h
@@ -20,18 +20,17 @@
 #endif
 
 //boot_flag
-#define R_BOOT_DEVICE_FLAG  READ_CBUS_REG(ASSIST_POR_CONFIG)
+//#define R_BOOT_DEVICE_FLAG  READ_CBUS_REG(ASSIST_POR_CONFIG)
 
-#if MESON_CPU_TYPE >= MESON_CPU_TYPE_MESON8
-#define POR_BOOT_VALUE 	((((R_BOOT_DEVICE_FLAG>>9)&1)<<2)|((R_BOOT_DEVICE_FLAG>>6)&3))
-#else
-#define POR_BOOT_VALUE 	(R_BOOT_DEVICE_FLAG & 7)
-#endif
+//#if MESON_CPU_TYPE >= MESON_CPU_TYPE_MESON8
+//#define POR_BOOT_VALUE 	((((R_BOOT_DEVICE_FLAG>>9)&1)<<2)|((R_BOOT_DEVICE_FLAG>>6)&3))
+//#else
+//#define POR_BOOT_VALUE 	(R_BOOT_DEVICE_FLAG & 7)
+//#endif
 
 //#if 1 /*defined in poc.h*/
 //#define POR_NAND_BOOT()	 ((POR_BOOT_VALUE == 7) || (POR_BOOT_VALUE == 6))
 //#define POR_SPI_BOOT()  		((POR_BOOT_VALUE == 5) || (POR_BOOT_VALUE == 4))
-//
 //#define POR_EMMC_BOOT()	((IS_MESON_M8M2_CPU | IS_MESON_M8BABY_CPU)?((POR_BOOT_VALUE == 3) || ((POR_BOOT_VALUE == 1))):(POR_BOOT_VALUE == 3))
 //#define POR_CARD_BOOT() 	(POR_BOOT_VALUE == 0)
 //#endif
