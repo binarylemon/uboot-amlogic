fe75146e4da5ae80c62f925fff6326d61c1e278e
Sam Wu <yihui.wu@amlogic.com>
2014-10-27 15:30:00 +0800

    PD#84625: upgrade support usb host device using autoscr
    In run recovery: after failed to connect PC and boot from external sdcard, load and run 'udisk_autoscript' from usb host disk

--- board/amlogic/configs/m8_k200_v1.h
+++ board/amlogic/configs/m8_k200_v1.h
@@ -352,6 +352,9 @@
         "if mmcinfo; then "\
             "if fatload mmc 0 ${loadaddr} recovery.img; then bootm;fi;"\
         "fi; "\
+        "if usb start 0; then "\
+                "if fatload usb 0 ${loadaddr} udisk_autoscript; then autoscr ${loadaddr}; fi;"\
+        "fi;"\
 	      "if imgread kernel recovery ${loadaddr}; then "\
 	        "bootm; "\
 				"else "\
