a9a5bcfffd8cc1472fc8188b4c83070233e7cbd5
Xiaobo Gu <xiaobo.gu@amlogic.com>
2015-04-22 13:27:40 +0800

    Update g9baby ddr detect function
    Change-Id: Ida60ea1426db1de69a75267100e175a6d71d8abc

--- arch/arm/cpu/aml_meson/g9b/firmware/ddr_auto_detect.c
+++ arch/arm/cpu/aml_meson/g9b/firmware/ddr_auto_detect.c
@@ -129,16 +129,6 @@ unsigned int dtar_reset(struct ddr_set * timing_reg){
 		case CFG_DDR_TWO_CHANNEL_SWITCH_BIT_8:
 			ddr_channel_switch = 0x1;
 			dtar_addr_0_offset = 0; dtar_addr_1_offset = 1<<8; break;
-		case CFG_DDR_TWO_CHANNEL_SWITCH_BIT_30:
-			if(timing_reg->phy_memory_size<=0x40000000){
-				serial_puts("DDR size lower than 1GB, doesn't support 2 channel switch bit30!\n");
-				AML_WATCH_DOG_START();
-				while(1);
-			}
-			else{
-				dtar_addr_0_offset = 0; dtar_addr_1_offset = 0x40000000;
-			}
-			break;
 		case CFG_DDR_TWO_CHANNEL_DDR0_LOW:
 			ddr_channel_switch = 0x2;
 			dtar_addr_0_offset = 0; dtar_addr_1_offset = ddr0_size; break;
