ee98771b44a8b1078e531c9eeca645b4294d5fa0
Liang Yang <liang.yang@amlogic.com>
2015-05-14 13:50:12 +0800

    PD105255: add support for toshiba 15nm 8&16G.
    Change-Id: Ib6f1c5b1c889d8b07fda725af70b43e819636187

--- drivers/amlnf/include/phynand.h
+++ drivers/amlnf/include/phynand.h
@@ -378,6 +378,7 @@ struct hw_controller;
 /* for Toshiba */
 #define	TOSHIBA_2XNM							20		/* TC58NVG5D2HTA00 */
 #define	TOSHIBA_A19NM							21																/* TC58NVG6D2GTA00 */
+#define TOSHIBA_15NM							22		//TC58TEG6DDLTA00
 /* for SAMSUNG */
 #define	SUMSUNG_2XNM							30
 
