f34161ff5eef97ea5d882256e34ec700a3b15da6
Sandy Luo <sandy.luo@amlogic.com>
2015-06-29 19:26:55 +0800

    PD#107187:g9bb n302:defenv after upgrade
    Change-Id: I3da15d8c8b0c718139cda3334f641ac23944cc5d

--- board/amlogic/configs/g9b_n302_v1.h
+++ board/amlogic/configs/g9b_n302_v1.h
@@ -200,8 +200,16 @@
 	"firstboot=1\0" \
 	"store=0\0"\
 	"sdcburncfg=aml_sdc_burn.ini\0"\
+	"upgrade_check="\
+                "if itest ${upgrade_step} == 3; then run prepare; run storeargs; run update; fi; "\
+                "if itest ${upgrade_step} == 1; then  "\
+                    "defenv; setenv upgrade_step 2; saveenv;"\
+                "fi; "\
+		"\0"\
 	"preboot="\
+		"run upgrade_check;"\
 		"run prepare; "\
+        "run storeargs;"\
 		"get_rebootmode; clear_rebootmode; echo reboot_mode=${reboot_mode}; "\
 		"run switch_bootmode\0" \
 		\
