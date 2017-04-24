e7754ae6a1d04dbff9838805bab204731790081a
Peipeng Zhao <peipeng.zhao@amlogic.com>
2017-04-24 14:49:17 +0800

    change emmc partition for M400
    Change-Id: Ia95189a389232b5fcad0db271c87a718e2caf3b1

--- board/amlogic/a111_m400_v1/firmware/storage.c
+++ board/amlogic/a111_m400_v1/firmware/storage.c
@@ -9,51 +9,25 @@ Header file: arch/arm/include/asm/arch-xx/storage.h
 //partition tables
 struct partitions partition_table[]={
 		{
-			.name = "logo",
-			.size = 32*SZ_1M,
-			.mask_flags = STORE_CODE,
-		},
-		{
-			.name = "recovery",
-			.size = 32*SZ_1M,
-			.mask_flags = STORE_CODE,
-		},
-		{
-			.name = "misc",
-			.size = 32*SZ_1M,
-			.mask_flags = STORE_CODE,
-		},
-#ifdef CONFIG_INSTABOOT
-		{
-			.name = "instaboot",
-			.size = 512*SZ_1M,
-			.mask_flags = STORE_CODE,
-		},
-#endif
-		{
 			.name = "boot",
-			.size = 32*SZ_1M,
+			.size = 9*SZ_1M,
 			.mask_flags = STORE_CODE,
 		},
 		{
 			.name = "system",
-			.size = 1024*SZ_1M,
+			.size = 152*SZ_1M,
 			.mask_flags = STORE_CODE,
 		},
 		{
-			.name = "cache",
-			.size = 512*SZ_1M,
-			.mask_flags = STORE_CACHE,
+			.name = "boot1",
+			.size = 9*SZ_1M,
+			.mask_flags = STORE_CODE,
 		},
 		{
-			.name = "data",
+			.name = "userdata",
 			.size = NAND_PART_SIZE_FULL,
 			.mask_flags = STORE_DATA,
 		},
-		/*{
-			.name = "media",
-			.size = NAND_PART_SIZE_FULL,
-		},*/
 };
 
 struct store_config  store_configs ={
