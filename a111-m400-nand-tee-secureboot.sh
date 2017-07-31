1518dfc450f98588880fc95f324df376904625d7
ivan.nie <ivan.nie@amlogic.com>
2017-07-31 16:35:02 +0800

    fix script issue for secure boot + secure os

--- a111-m400-nand-tee-secureboot.sh
+++ a111-m400-nand-tee-secureboot.sh
@@ -3,6 +3,17 @@ make distclean
 make a111_m400_nand_tee_config
 make -j8
 
+# burn to nand/emmc
 cp  -v build/uboot-secureos.bin.aml.encrypt  ../../output/meson8b_m400/images/u-boot.bin.aml.encrypt
+
+# no encrypt
 cp  -v build/ddr_init.bin  ../../output/meson8b_m400/images/
 cp  -v build/uboot-secureos.bin  ../../output/meson8b_m400/images/u-boot-comp.bin
+
+# encrypt
+cp  -v build/u-boot-usb.bin.aml.encrypt.usb.start  ../../output/meson8b_m400/images/
+cp  -v build/u-boot-usb.bin.aml.encrypt  ../../output/meson8b_m400/images/
+
+# burn to efuse
+cp  -v build/u-boot.bin.aml.efuse  ../../output/meson8b_m400/images/u-boot.bin.aml.efuse
+
